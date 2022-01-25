f1696d781a257ddafc0047752049d38b11ac3b42
namhnz <namqhong@gmail.com>
2022-01-25 21:32:21 +0700

    them repos thong tin chinh sua ho so

--- QuanLyTangThuHoKhau.Core/Database/DataReposNames.cs
+++ QuanLyTangThuHoKhau.Core/Database/DataReposNames.cs
@@ -5,5 +5,6 @@
         public const string CAC_TAP_HSCT = "CAC_TAP_HSCT";
         public const string CAC_TUI_HSCT = "CAC_TUI_HSCT";
         public const string CAC_THON_XOM = "CAC_THON_XOM";
+        public const string CAC_CHINH_SUA_HSCT = "CAC_CHINH_SUA_HSCT";
     }
 }
---
+++ QuanLyTangThuHoKhau.Core/DbRepositories/ThayDoiThongTinRepos/ILiteDbChinhSuaHSCTRepository.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using QuanLyTangThuHoKhau.Core.Models;
+
+namespace QuanLyTangThuHoKhau.Core.DbRepositories.ThayDoiThongTinRepos
+{
+    public interface ILiteDbChinhSuaHSCTRepository
+    {
+        #region Tim kiem
+
+        public IEnumerable<ChinhSuaHSCT> FindAll();
+
+        public ChinhSuaHSCT FindOne(int id);
+
+        #endregion
+
+        #region Them moi
+
+        public bool Insert(ChinhSuaHSCT chinhSuaHSCT);
+        public int InsertMany(List<ChinhSuaHSCT> cacChinhSuaHSCT);
+
+        #endregion
+
+        #region Chinh sua
+
+        // public bool Update(ChinhSuaHSCT chinhSuaHSCT);
+
+        #endregion
+
+        #region Xoa
+
+        // public bool Delete(int id);
+        public int DeleteAll();
+
+        #endregion
+    }
+}
